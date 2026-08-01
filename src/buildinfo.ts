77969c1c7eddb2219e1e049957aabb431bb30d0f
fwzjszd <qz750465491@163.com>
2026-08-01 14:51:51 +0800

    源码第三次同步，与next-gen分支同步。

--- src/buildinfo.ts
+++ src/buildinfo.ts
@@ -1,5 +1,5 @@
 
-  export const GIT_COMMIT_HASH = "5f8ce63ebd7220f479bdaca3ca945261d6070925";
-  export const BUILD_TIME = "2026-08-01T06:39:14.022Z";
+  export const GIT_COMMIT_HASH = "53b7e67c96c9fb77da33d3379454623f57394917";
+  export const BUILD_TIME = "2026-08-01T06:46:21.607Z";
   export const BUILD_USER = "Bob";
-  export const DESIGN_WIDTH = device-width;
+  export const DESIGN_WIDTH = "device-width";
